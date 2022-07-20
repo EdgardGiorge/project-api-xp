ebe87e98eda62a79d3f1829e6b9bf86fe2759d78
EdgardGiorge <edgardgiorge@hotmail.com>
2022-07-20 00:18:32 -0300

    feat: up app local

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import 'express-async-errors';
+
+import routes from './routes'
+
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+export default app;
