59b1468c809895900c490167611c30619e097336
EdgardGiorge <edgardgiorge@hotmail.com>
2022-07-20 23:11:27 -0300

    fix:rename ativos route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import assetsRoute from './assetsRoute'
+import clientRoute from "./clientRoute";
 import countRoute from "./countRoute";
 
 const routes = express.Router();
 
 routes.use('/ativos', assetsRoute);
 routes.use('/conta', countRoute);
+routes.use('/ativoscliente', clientRoute);
 
 export default routes;
